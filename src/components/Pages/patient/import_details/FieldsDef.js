cd365aeff957edbf7a1537a1fd7b2ff8ffef1ac2
Ashraful Dowla <ashrafuldowlaunited532@gmail.com>
2019-08-23 23:31:14 +0600

    laravue update

---
+++ src/components/Pages/patient/import_details/FieldsDef.js
@@ -0,0 +1,37 @@
+export const FieldsDef= [
+		{
+			name:'id',
+			title:'Patient ID',
+			sortField: 'id'
+		},
+		{
+			name:'name',
+			title: '<i class="fa fa-tag"></i> Patient Name',
+			sortField: 'name'
+		},
+		{
+			name:'name',
+			title: '<i class="fa fa-tag"></i> Doctor Name',
+			sortField: 'name'
+		},
+		{
+			name:'name',
+			title: '<i class="fa fa-tag"></i> Specialist',
+			sortField: 'name'
+		},
+		{
+			name:'birthdate',
+			title: 'Last Visit',
+		    sortField: 'birthdate',
+		    titleClass: 'center aligned',
+		    dataClass: 'center aligned',
+		},
+		{
+			name:'birthdate',
+			title: 'Next Visit',
+		    sortField: 'birthdate',
+		    titleClass: 'center aligned',
+		    dataClass: 'center aligned',
+		},
+		'actions'
+] 
---
+++ src/components/Pages/patient/import_details/FieldsDef_prescription.js
@@ -0,0 +1,35 @@
+export const FieldsDef_prescription= [
+		{
+			name:'id',
+			title:'Patient ID',
+			sortField: 'id'
+		},
+		{
+			name:'name',
+			title: '<i class="fa fa-tag"></i> Patient Name',
+			sortField: 'name'
+		},
+		{
+			name:'name',
+			title: '<i class="fa fa-tag"></i> Doctor Name',
+			sortField: 'name'
+		},
+		{
+			name:'name',
+			title: '<i class="fa fa-tag"></i> Specialist',
+			sortField: 'name'
+		},
+		{
+			name:'birthdate',
+			title: 'Visited date',
+		    sortField: 'birthdate',
+		    titleClass: 'center aligned',
+		    dataClass: 'center aligned',
+		},
+		{
+			name:'name',
+			title: '<i class="fa fa-tag"></i> Prescription',
+			sortField: 'name'
+		},
+		'actions'
+] 
